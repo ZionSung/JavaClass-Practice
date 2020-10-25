ae54749c51416418c3f776f1ef6242efbf57eb96
Zion Sung <1122overcomer@gmail.com>
2020-10-25 22:02:23 +0800

    Enter

---
+++ three1.java
@@ -0,0 +1,14 @@
+/*
+   由鍵盤輸入一字串，可包含空白及符號，輸出為該字串的長度
+*/
+
+
+import java.util.*;
+
+class three1{
+    public static void main( String[] args ){
+        Scanner sc = new Scanner(System.in);
+        String in = sc.nextLine();
+        System.out.println(in.length());
+    }
+} 
