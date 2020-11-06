42b6144d2a45dc67279ffb3c4af9d9ca16a8e6a4
ZionSung <1122overcomer@gmail.com>
2020-11-06 16:38:31 +0800

    enter

---
+++ ch4/Student.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Student {
+    int age = 8;
+    int grade = 0;
+    String name = null;
+    String id = null;
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
---
+++ ch4/class_def.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class class_def{
+    public static void main( String[] args ){
+        Student stu = new Student();
+        int test = stu.getAge();
+        System.out.println(test);
+    }
+}
+
+
+
+
+
+
